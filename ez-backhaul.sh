994c2de7a9f09b0a36e77241e63cd6387c4a18b7
mikeesierrah <mike@mike.mike>
2024-09-16 00:32:02 +0330

    modified:   ez-backhaul.sh

--- ez-backhaul.sh
+++ ez-backhaul.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
-URL="https://github.com/mikeesierrah/ez-backhaul/raw/main/ez-backhaul.sh.enc"
 
-echo "Downloading and decrypting the script..."
-curl -Ls "$URL" | openssl enc -aes-256-cbc -d -pass pass:"$(read -sp 'Enter decryption password: ' PASSWORD && echo $PASSWORD)" | bash
-if [ $? -ne 0 ]; then
-    echo "Failed to decrypt or execute the script."
+
+ENCRYPTED_SCRIPT_URL="https://raw.githubusercontent.com/mikeesierrah/ez-backhaul/main/ez-backhaul.sh.enc"
+
+echo "Downloading encrypted script..."
+TEMP_ENCRYPTED_SCRIPT=$(mktemp)
+curl -Ls "$ENCRYPTED_SCRIPT_URL" -o "$TEMP_ENCRYPTED_SCRIPT"
+
+
+if [ ! -f "$TEMP_ENCRYPTED_SCRIPT" ]; then
+    echo "Failed to download the encrypted script."
     exit 1
 fi
+
+echo -n "Enter decryption password: "
+read -s PASSWORD
+echo
+
+echo "Decrypting and executing the script..."
+openssl enc -aes-256-cbc -d -in "$TEMP_ENCRYPTED_SCRIPT" -pass pass:"$PASSWORD" | bash
+
+if [ $? -ne 0 ]; then
+    echo "Invalid password or decryption failed."
+    rm "$TEMP_ENCRYPTED_SCRIPT"
+    exit 1
+fi
+
+rm "$TEMP_ENCRYPTED_SCRIPT"
